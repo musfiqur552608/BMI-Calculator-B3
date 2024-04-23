ad8b55e1ce5d49f387b6a0391dd619fd583b7055
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2024-04-23 12:19:05 +0600

    Bmi calculator done

--- app/src/main/java/org/freedu/bmicalculator/MainActivity.kt
+++ app/src/main/java/org/freedu/bmicalculator/MainActivity.kt
@@ -1,6 +1,9 @@
 package org.freedu.bmicalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@MainActivity, BMIActivity::class.java))
+        }, 3000)
 
     }
 }
